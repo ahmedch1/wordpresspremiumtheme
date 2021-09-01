01ad8aeda476630ee7a0faac37526b5b4dad1af9
Ahmed Ch <ahmedchouihi@gmail.com>
2021-09-01 08:56:45 +0100

    'functionsInWordpress'

--- wp-content/themes/firsttheme/index.php
+++ wp-content/themes/firsttheme/index.php
@@ -1,9 +1,6 @@
 <?php get_header(); ?>
-<pre>
-<?php var_dump($wp_query); ?>
-</pre>
 <?php if ($wp_query->have_posts()) { ?>
-
+    git
     <?php while ($wp_query->have_posts()) { ?>
         <?php $wp_query->the_post(); ?>
         <h2>
@@ -12,22 +9,12 @@
             </a>
         </h2>
         <div>
-            Posted on <a href="<?php echo get_permalink() ?>">
-                <time datetime="<?php echo get_the_date() ?>">
-                    <?php echo get_the_date('l,F j, Y') ?>
-                </time>
-            </a>
-            By <a href="<?php echo get_author_posts_url(get_the_author_meta('ID')) ?>">
-                <?php echo get_the_author(); ?>
-            </a>
+            <?php firsttheme_post_meta(); ?>
         </div>
         <div>
             <?php the_excerpt(); ?>
         </div>
-        <a href="<?php echo get_the_permalink() ?>" title="<?php the_title_attribute(); ?>">
-            Read More <span class="u-screen-reader-text">About<?php the_title() ?></span>
-        </a>
-
+        <?php firsttheme_readmore_link(); ?>
     <?php } ?>
     <?php the_posts_pagination(); ?>
 <?php } else {
