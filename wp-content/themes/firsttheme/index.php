53aed1bb01749c5b888d1c3807c98dd7cf2b4b8a
Ahmed Ch <ahmedchouihi@gmail.com>
2021-09-01 11:15:36 +0100

    'internationalizationAndLocalization'

--- wp-content/themes/firsttheme/index.php
+++ wp-content/themes/firsttheme/index.php
@@ -1,6 +1,5 @@
 <?php get_header(); ?>
 <?php if ($wp_query->have_posts()) { ?>
-    git
     <?php while ($wp_query->have_posts()) { ?>
         <?php $wp_query->the_post(); ?>
         <h2>
@@ -19,7 +18,19 @@
     <?php the_posts_pagination(); ?>
 <?php } else {
     ?>
-    <p>Sorry,no posts</p>
+    <p><?php echo esc_html_e('Sorry,no posts matched your criteria', 'firsttheme'); ?></p>
 <?php } ?>
+
+<?php
+$city = 'london';
+echo esc_html__('Your city is ', 'firsttheme') . $city;
+
+printf(
+        /* translators: %s is the city name */
+    esc_html('Your city is %s', 'firsttheme'),
+    $city
+)
+
+?>
 <?php get_footer(); ?>
 
