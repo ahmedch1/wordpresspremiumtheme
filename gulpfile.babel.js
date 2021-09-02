d972264cd465f769b5f6dad53c696db46bf8f33e
Ahmed Ch <ahmedchouihi@gmail.com>
2021-09-02 11:10:13 +0100

    'creatingAGulpTaskForStylesCont'

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -1,31 +1,28 @@
-// var gulp = require('gulp');
-//
-// gulp.task('default', defaultTask);
-//
-// function defaultTask(done) {
-//     // place code for your default task here
-//     console.log('hey');
-//     done();
-// }
-//
-// gulp.task('hello', function (done) {
-//     console.log('hello');
-//     done();
-// })
-
 import gulp from 'gulp';
 import yargs from 'yargs';
 import cleanCSS from 'gulp-clean-css';
 import gulpif from 'gulp-if';
+import sourcemaps from 'gulp-sourcemaps';
 
 const sass = require('gulp-sass')(require('sass'));
 const PRODUCTION = yargs.argv.prod;
 
-export const styles = () => {
-    return gulp.src('src/assets/scss/bundle.scss')
-        .pipe(sass().on('error', sass.logError))
-        .pipe(gulpif(PRODUCTION, cleanCSS({compatibility: 'ie8'})))
-        .pipe(gulp.dest('dist/asset/css'));
+const paths = {
+    styles: {
+        src: ['src/assets/scss/bundle.scss', 'src/assets/scss/admin.scss'],
+        dest: 'dist/asset/css'
+    }
 }
 
-// export default styles;
+export const styles = () => {
+    return gulp.src(paths.styles.src)
+        .pipe(gulpif(!PRODUCTION, sourcemaps.init()))
+        .pipe(sass().on('error', sass.logError))
+        .pipe(gulpif(PRODUCTION, cleanCSS({compatibility: 'ie8'})))
+        .pipe(gulpif(!PRODUCTION, sourcemaps.write()))
+        .pipe(gulp.dest(paths.styles.dest));
+}
+
+export const watch = () => {
+    gulp.watch('src/assets/scss/**/*.scss',styles);
+}
--- wp-content/themes/firsttheme/lib/enqueue-assets.php
+++ wp-content/themes/firsttheme/lib/enqueue-assets.php
@@ -1,9 +1,18 @@
 <?php
 function firsttheme_assets()
 {
-    wp_enqueue_style('firsttheme-stylesheet',  '/dist/assets/css/bundle.css',
+    wp_enqueue_style('firsttheme-stylesheet',  '/dist/asset/css/bundle.css',
         array(), '1.0.0', 'all'
     );
 }
 
 add_action('wp_enqueue_scripts', 'firsttheme_assets');
+
+function firsttheme_admin_assets()
+{
+    wp_enqueue_style('firsttheme-admin-stylesheet',  '/dist/asset/css/admin.css',
+        array(), '1.0.0', 'all'
+    );
+}
+
+add_action('admin_enqueue_scripts', 'firsttheme_assets');
