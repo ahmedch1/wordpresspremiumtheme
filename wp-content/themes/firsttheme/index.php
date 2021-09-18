426815e55e42896084a8a94f8f6a0eb60290a50a
Ahmed Ch <ahmedchouihi@gmail.com>
2021-09-18 18:43:04 +0100

    'creatingSidebarsForWidgets'

--- wp-content/themes/firsttheme/index.php
+++ wp-content/themes/firsttheme/index.php
@@ -1,7 +1,8 @@
 <?php get_header(); ?>
 <div class="o-container u-margin-bottom-40">
     <div class="o-row">
-        <div class="o-row__column o-row__column--span-12 o-row__column--span-8@medium">
+        <div class="o-row__column o-row__column--span-12 o-row__column--span-<?php is_active_sidebar('primary-sidebar') ?
+            '8' : '12' ?>@medium">
             <main role="main">
                 <?php if ($wp_query->have_posts()) { ?>
                     <?php while ($wp_query->have_posts()) { ?>
@@ -31,9 +32,11 @@
                 <?php } ?>
             </main>
         </div>
-        <div class="o-row__column o-row__column--span-12 o-row__column--span-4@medium">
-            <?php get_sidebar(); ?>
-        </div>
+        <?php if (is_active_sidebar('primary-sidebar')) { ?>
+            <div class="o-row__column o-row__column--span-12 o-row__column--span-4@medium">
+                <?php get_sidebar(); ?>
+            </div>
+        <?php } ?>
     </div>
 </div>
 <?php get_footer(); ?>
