701a2be4960135558aae6b802dbd4f7d95b9e4ee
Ahmed Ch <ahmedchouihi@gmail.com>
2021-09-22 10:06:22 +0100

    'creatingSidebarsDynamically'

--- wp-content/themes/firsttheme/lib/sidebars.php
+++ wp-content/themes/firsttheme/lib/sidebars.php
@@ -6,9 +6,32 @@ function _themename_sidebar_widgets()
         'name' => esc_html__('Primary Sidebar', '_themename'),
         'description' => esc_html__('This sidebar appears in the blog posts page', '_themename'),
         'before_widget' => '<section id="%1s" class="c-sidebar-widget u-margin-bottom-20 %2$s">',
-        'after_widget'=>'</section>',
-        'before_title'=>'<h5>',
-        'after_title'=>'</h5>'
+        'after_widget' => '</section>',
+        'before_title' => '<h5>',
+        'after_title' => '</h5>'
+    ));
+}
+
+$footer_layout = '3,3,3,3';
+$columns = explode(',', $footer_layout);
+$footer_bg = 'light';
+$widget_theme = '';
+if ($footer_bg == 'light') {
+    $widget_theme = 'c-footer-widget--dark';
+} else {
+    $widget_theme = 'c-footer-widget--light';
+}
+
+
+foreach ($columns as $i => $column) {
+    register_sidebar(array(
+        'id' => 'footer-sidebar-' . ($i + 1),
+        'name' => sprintf(esc_html__('Footer Widgets Column %', '_themename'), $i + 1),
+        'description' => esc_html__('Footer widgets', '_themename'),
+        'before_widget' => '<section id="%1s" class="c-footer-widget %2$s' . $widget_theme . '%22$s">',
+        'after_widget' => '</section>',
+        'before_title' => '<h5>',
+        'after_title' => '</h5>'
     ));
 }
 
